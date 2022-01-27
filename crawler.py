c6ca50b96aaff1ec60bd420698a37b75c65cb185
MrHr6874 <harshrathod6874@gmail.com>
2022-01-27 13:39:10 +0530

    commit on 'master'

---
+++ crawler.py
@@ -0,0 +1,118 @@
+from tkinter import *
+import tkinter as tk
+from tkinter import messagebox as ms
+from PIL import ImageTk, Image
+from bs4 import BeautifulSoup
+import urllib.request
+
+def Scrape(arg=None):
+    
+
+    if url_entry.get() == '':
+        ms.showerror('Oops', 'Enter A Valid URL !!!')
+        
+    else:
+        try:
+            
+            
+            url = url_entry.get()
+            
+            
+            content = urllib.request.urlopen(url).read()
+            
+            
+            soup = BeautifulSoup(content, features="lxml")
+
+            
+            info = soup.prettify()
+            
+
+
+            root = tk.Toplevel()
+
+            
+            root.title('Thank You For Using Our Service !!!!')
+
+            
+            root.iconbitmap('img/logo.ico')
+
+            
+            root.resizable(width=False, height=False)
+            
+            
+            
+            
+            scrollbar = Scrollbar(root)
+            scrollbar.pack(side=RIGHT, fill=Y)
+
+            
+            text = Text(root, yscrollcommand=scrollbar.set, wrap = WORD)
+            text.insert(INSERT, info)
+            text.pack()
+
+            
+            scrollbar.config(command=text.yview)
+
+        except ValueError:
+            ms.showerror('Error', 'Enter A Valid URL !!!')
+    
+
+
+crawler = tk.Tk()
+
+
+crawler.geometry('600x400')
+
+
+crawler.resizable(width=False, height=False)
+
+
+crawler.title('Web Crawler')
+
+
+crawler.iconbitmap('img/logo.ico')
+''' Window Setting End '''
+
+
+top_frame = Label(crawler, text='WEB SCRAPER',font = ('Courier New', 25, 'bold'), bg='#2980B9', fg='white', relief='groove',padx=500, pady=30, bd='5')
+top_frame.pack(side='top')
+
+
+
+canvas = Canvas(crawler, width=500, height=500)
+
+
+image = ImageTk.PhotoImage(Image.open('img/bg6.jpg'))
+
+
+canvas.create_image(0,0, anchor=NW, image=image)
+canvas.pack()
+
+
+frame = LabelFrame(crawler, padx=30, pady=40, bg='white', bd='5', relief='groove')
+frame.place(relx = 0.5, rely = 0.5, anchor = CENTER)
+
+ 
+url_add = tk.Label(frame, text = 'Enter a URL or Web Address',font=('Courier New',10, 'bold'),bg='white', fg='green').pack()
+
+url_entry = tk.Entry(frame, font=('Courier New',10,'normal'), justify = 'center', bg='#FBB13C', width='30')
+
+
+url_entry.bind('<Return>', Scrape)
+
+
+url_entry.focus_set()
+
+
+url_entry.pack()
+
+
+label = Label(frame, bg='white').pack()
+
+
+crawl = tk.Button(frame, text = "Scrape", width="10", bd = '3', command = Scrape, font = ('Courier New', 12, 'bold'), bg='#7268A6',relief='groove', justify = 'center', pady='5').pack()
+
+
+crawler.mainloop()
+
+ 
---
+++ main.py
@@ -0,0 +1,62 @@
+import tkinter as tk
+from tkinter import *
+import os
+#from turtle import bgcolor
+
+descryption = '''CyberSecurity Tool developed by Group 2
+It's an open Source project.
+Tools included NetworkScanner and WebCrawler'''
+
+def WindowConfiguration(tk,windowtitle):
+    windowWidth = 600
+    windowHeight = 400
+
+    screenWidth = tk.winfo_screenwidth()
+    screenHeight = tk.winfo_screenheight()
+
+    centreX = int(screenWidth / 2 - windowWidth / 2)
+    centreY = int(screenHeight / 2 - windowHeight / 2)
+
+    defaultGeometry = f'{windowWidth}x{windowHeight}+{centreX}+{centreY}'
+    
+    tk.geometry(defaultGeometry)
+    tk.config(bg='#BDC3C7')
+    tk.resizable(False, False)
+    tk.title(windowtitle)
+    tk.attributes('-alpha', 1)
+    #tk.iconbitmap('image.ico')
+
+    
+NetworkScannerFrame = None
+
+class App(tk.Tk):
+    def __init__(self):
+        super().__init__()
+        
+        WindowConfiguration(self,"CyberSecurity Multi-Tool")
+        self.DisplayToolDescryption()
+        self.CreateMenuButtons()
+
+    def DisplayToolDescryption(self):
+        global descryption
+        descryptionLabel = Label(self,text = descryption, bg='#666699', fg='white')
+        descryptionLabel.pack()
+
+    def OnNetworkScannerButtonClick(self):
+        '''global NetworkScannerFrame
+        NetworkScannerFrame = tk.Toplevel(self)
+        WindowConfiguration(NetworkScannerFrame, windowtitle='NetWorkScanner')'''
+        os.system("python3 NetworkScanner.py")
+
+    def OnWebCrawlerButtonClick(self):
+        os.system("python3 crawler.py")
+
+    def CreateMenuButtons(self):
+        networkScannerBtn = tk.Button(self,text='Network Scanner', command=lambda: self.OnNetworkScannerButtonClick(), bg='#2980B9', fg='white')
+        networkScannerBtn.pack(side='left', padx=20, pady=20)
+        networkScannerBtn = tk.Button(self,text='Web Crawler', command=lambda: self.OnWebCrawlerButtonClick(),bg='#2980B9', fg='white')
+        networkScannerBtn.pack(side='right', padx=20, pady=20)
+
+if __name__ == '__main__':
+    app = App()
+    app.mainloop()
